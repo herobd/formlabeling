c89c56406692b19206e24e20099921ed5cbbc556
Brian Davis <herobd@gmail.com>
2018-01-19 16:07:51 -0700

    UI for template labeling seems to be working

--- keylabeler.py
+++ keylabeler.py
@@ -20,32 +20,40 @@ def clicker(event, x, y, flags, p):
             if abs((p.startX-p.endX)*(p.startY-p.endY))>10:
                 didPair=None
                 if 'text' in p.mode and 'field' in p.selected:
-                    p.pairing.append((len(p.textBBs),p.selectedIdx))
-                    didPair=(len(p.textBBs),p.selectedIdx)
+                    p.pairing.append((p.textBBsCurId,p.selectedId))
+                    didPair=[(p.textBBsCurId,p.selectedId)]
                 elif 'field' in p.mode and 'text' in p.selected:
-                    p.pairing.append((p.selectedIdx,len(p.fieldBBs)))
-                    didPair=(p.selectedIdx,len(p.fieldBBs))
+                    p.pairing.append((p.selectedId,p.fieldBBsCurId))
+                    didPair=[(p.selectedId,p.fieldBBsCurId)]
 
                 if p.mode=='text':
-                    p.textBBs.append((p.startX,p.startY,p.endX,p.endY,0))
-                    p.actionStack.append(('append-text',p.startX,p.startY,p.endX,p.endY,0,didPair))
-                    p.selectedIdx=len(p.textBBs)-1
+                    p.textBBs[p.textBBsCurId]=(p.startX,p.startY,p.endX,p.endY,0)
+                    p.actionStack.append(('add-text',p.textBBsCurId,p.startX,p.startY,p.endX,p.endY,0,didPair))
+                    p.undoStack=[]
+                    p.selectedId=p.textBBsCurId
                     p.selected='text'
+                    p.textBBsCurId+=1
                 elif p.mode=='textP':
-                    p.textBBs.append((p.startX,p.startY,p.endX,p.endY,1))
-                    p.actionStack.append(('append-textP',p.startX,p.startY,p.endX,p.endY,1,didPair))
-                    p.selectedIdx=len(p.textBBs)-1
+                    p.textBBs[p.textBBsCurId]=(p.startX,p.startY,p.endX,p.endY,1)
+                    p.actionStack.append(('add-text',p.textBBsCurId,p.startX,p.startY,p.endX,p.endY,1,didPair))
+                    p.undoStack=[]
+                    p.selectedId=p.textBBsCurId
                     p.selected='text'
+                    p.textBBsCurId+=1
                 elif p.mode=='field':
-                    p.fieldBBs.append((p.startX,p.startY,p.endX,p.endY,0))
-                    p.actionStack.append(('append-field',p.startX,p.startY,p.endX,p.endY,0,didPair))
-                    p.selectedIdx=len(p.fieldBBs)-1
+                    p.fieldBBs[p.fieldBBsCurId]=(p.startX,p.startY,p.endX,p.endY,0)
+                    p.actionStack.append(('add-field',p.fieldBBsCurId,p.startX,p.startY,p.endX,p.endY,0,didPair))
+                    p.undoStack=[]
+                    p.selectedId=p.fieldBBsCurId
                     p.selected='field'
+                    p.fieldBBsCurId+=1
                 elif p.mode=='fieldP':
-                    p.fieldBBs.append((p.startX,p.startY,p.endX,p.endY,1))
-                    p.actionStack.append(('append-fieldP',p.startX,p.startY,p.endX,p.endY,1,didPair))
-                    p.selectedIdx=len(p.fieldBBs)-1
+                    p.fieldBBs[p.fieldBBsCurId]=(p.startX,p.startY,p.endX,p.endY,1)
+                    p.actionStack.append(('add-field',p.fieldBBsCurId,p.startX,p.startY,p.endX,p.endY,1,didPair))
+                    p.undoStack=[]
+                    p.selectedId=p.fieldBBsCurId
                     p.selected='field'
+                    p.fieldBBsCurId+=1
             draw(p)
         else:
             if '-d' == p.mode[-2:]:
@@ -61,43 +69,62 @@ def clicker(event, x, y, flags, p):
 
                     if x>=min(x1,x2) and x<=max(x1,x2) and y>=min(y1,y2) and y<=max(y1,y2) and abs((y2-y1)*x - (x2-x1)*y + x2*y1 - y2*x1)/math.sqrt(pow(y2-y1,2.0) + pow(x2-x1,2.0)) < 3.5:
                         p.actionStack.append(('remove-pairing',text,field))
+                        p.undoStack=[]
                         del p.pairing[index]
                         draw(p)
                         return
 
-            for index, (startX,startY,endX,endY,para) in enumerate(p.textBBs):
+            for id, (startX,startY,endX,endY,para) in p.textBBs.iteritems():
                 if x>=startX and x<=endX and y>=startY and y<=endY:
                     if p.mode=='delete':
-                        #TODO populate pairs and delete them too
-                        p.actionStack.append(('remove-text',startX,startY,endX,endY,para,pairs))
-                        del p.textBBs[index]
-                        if p.selected=='text' and p.selectedIdx==index:
+                        pairs=[]#pairs this BB is part of
+                        for pair in p.pairing:
+                            if id==pair[0]:
+                                pairs.append(pair)
+                        for pair in pairs:
+                            p.pairing.remove(pair)
+                        p.actionStack.append(('remove-text',id,startX,startY,endX,endY,para,pairs))
+                        p.undoStack=[]
+                        del p.textBBs[id]
+                        if p.selected=='text' and p.selectedId==id:
                             p.selected='none'
                     else:
-                        if p.selected=='field' and (index,p.selectedIdx) not in p.pairing:
-                            p.pairing.append((index,p.selectedIdx))
-                            p.actionStack.append(('append-pairing',index,p.selectedIdx))
-                        p.selectedIdx=index
+                        if p.selected=='field' and (id,p.selectedId) not in p.pairing:
+                            p.pairing.append((id,p.selectedId))
+                            p.actionStack.append(('add-pairing',id,p.selectedId))
+                            p.undoStack=[]
+                        p.selectedId=id
                         p.selected='text'
                     draw(p)
                     return
 
-            for index, (startX,startY,endX,endY,para) in enumerate(p.fieldBBs):
+            for id, (startX,startY,endX,endY,para) in p.fieldBBs.iteritems():
                 if x>=startX and x<=endX and y>=startY and y<=endY:
                     if p.mode=='delete':
-                        #TODO populate pairs and delete them too
-                        p.actionStack.append(('remove-field',startX,startY,endX,endY,para,pairs))
-                        del p.textBBs[index]
-                        if p.selected=='field' and p.selectedIdx==index:
+                        pairs=[]#pairs this BB is part of
+                        for pair in p.pairing:
+                            if id==pair[1]:
+                                pairs.append(pair)
+                        for pair in pairs:
+                            p.pairing.remove(pair)
+                        p.actionStack.append(('remove-field',id,startX,startY,endX,endY,para,pairs))
+                        p.undoStack=[]
+                        del p.fieldBBs[id]
+                        if p.selected=='field' and p.selectedId==id:
                             p.selected='none'
                     else:
-                        if p.selected=='text' and (p.selectedIdx,index) not in p.pairing:
-                            p.pairing.append((p.selectedIdx,index))
-                            p.actionStack.append(('append-pairing',p.selectedIdx,index))
-                        p.selectedIdx=index
+                        if p.selected=='text' and (p.selectedId,id) not in p.pairing:
+                            p.pairing.append((p.selectedId,id))
+                            p.actionStack.append(('add-pairing',p.selectedId,id))
+                            p.undoStack=[]
+                        p.selectedId=id
                         p.selected='field'
                     draw(p)
                     return
+
+            if p.selected!='none':
+                p.selected='none'
+                draw(p)
 
             
     elif event == cv2.EVENT_MOUSEMOVE:
@@ -110,8 +137,10 @@ def clicker(event, x, y, flags, p):
 
 class Params:
     mode='none'
-    textBBs=[]
-    fieldBBs=[]
+    textBBs={}
+    textBBsCurId=0
+    fieldBBs={}
+    fieldBBsCurId=0
     pairing=[]
     image=None
     displayImage=None
@@ -121,19 +150,77 @@ class Params:
     endY=-1
     actionStack=[]
     undoStack=[]
-    selectedIdx=-1
+    selectedId=-1
     selected='none'
+
+def undo(p):
+    if len(p.actionStack)>0:
+        action = p.actionStack.pop()
+        action = undoAction(p,action)
+
+        p.undoStack.append(action)
+        draw(p)
+
+def redo(p):
+    if len(p.undoStack)>0:
+        action = p.undoStack.pop()
+        action = undoAction(p,action)
+
+        p.actionStack.append(action)
+        draw(p)
+
+def undoAction(p,action):
+    if action[0] == 'add-pairing':
+        p.pairing.remove((action[1],action[2]))
+        return ('remove-pairing',action[1],action[2])
+    elif action[0] == 'remove-pairing':
+        p.pairing.append((action[1],action[2]))
+        return ('add-pairing',action[1],action[2])
+    elif action[0] == 'add-text':
+        label,id,startX,startY,endX,endY,para,pairs = action
+        del p.textBBs[id]
+        if pairs is not None:
+            for pair in pairs:
+                p.pairing.remove(pair)
+        if p.selected=='text' and p.selectedId==id:
+            p.selected='none'
+        return ('remove-text',id,startX,startY,endX,endY,para,pairs)
+    elif action[0] == 'remove-text':
+        label,id,startX,startY,endX,endY,para,pairs = action
+        p.textBBs[id]=(startX,startY,endX,endY,para)
+        if pairs is not None:
+            for pair in pairs:
+                p.pairing.append(pair)
+        return ('add-text',id,startX,startY,endX,endY,para,pairs)
+    elif action[0] == 'add-field':
+        label,id,startX,startY,endX,endY,para,pairs = action
+        del p.fieldBBs[id]
+        if pairs is not None:
+            for pair in pairs:
+                p.pairing.remove(pair)
+        if p.selected=='field' and p.selectedId==id:
+            p.selected='none'
+        return ('remove-field',id,startX,startY,endX,endY,para,pairs)
+    elif action[0] == 'remove-field':
+        label,id,startX,startY,endX,endY,para,pairs = action
+        p.fieldBBs[id]=(startX,startY,endX,endY,para)
+        if pairs is not None:
+            for pair in pairs:
+                p.pairing.append(pair)
+        return ('add-field',id,startX,startY,endX,endY,para,pairs)
+    else:
+        print 'Unimplemented action: '+action[0]
 
 def draw(p):
     p.displayImage[0:p.image.shape[0], 0:p.image.shape[1]] = p.image
-    for startX,startY,endX,endY,para in p.textBBs:
+    for id, (startX,startY,endX,endY,para) in p.textBBs.iteritems():
         if para:
             g=150
         else:
             g=0
         cv2.rectangle(p.displayImage,(startX,startY),(endX,endY),(255,g,0),1)
 
-    for startX,startY,endX,endY,para in p.fieldBBs:
+    for id, (startX,startY,endX,endY,para) in p.fieldBBs.iteritems():
         if para:
             g=120
         else:
@@ -148,10 +235,10 @@ def draw(p):
         cv2.line(p.displayImage,(x1,y1),(x2,y2),(0,255,0),1)
 
     if p.selected == 'text':
-        startX,startY,endX,endY,para = p.textBBs[p.selectedIdx]
+        startX,startY,endX,endY,para = p.textBBs[p.selectedId]
         cv2.rectangle(p.displayImage,(min(startX,endX)-2,min(startY,endY)-2),(max(startX,endX)+2,max(startY,endY)+2),(255,0,255),1)
     elif p.selected == 'field':
-        startX,startY,endX,endY,para = p.fieldBBs[p.selectedIdx]
+        startX,startY,endX,endY,para = p.fieldBBs[p.selectedId]
         cv2.rectangle(p.displayImage,(min(startX,endX)-2,min(startY,endY)-2),(max(startX,endX)+2,max(startY,endY)+2),(255,0,255),1)
 
     if p.mode[-2:]=='-m':
@@ -207,6 +294,16 @@ def drawToolbar(p):
     cv2.putText(p.displayImage,'5:delete',(p.displayImage.shape[0]-TOOL_WIDTH+3,y+toolH-3),cv2.FONT_HERSHEY_PLAIN,2.0,(40,40,40))
     y+=toolH+1
 
+    #undo
+    p.displayImage[y:y+toolH,-TOOL_WIDTH:]=(160,160,160)
+    cv2.putText(p.displayImage,'6:undo',(p.displayImage.shape[0]-TOOL_WIDTH+3,y+toolH-3),cv2.FONT_HERSHEY_PLAIN,2.0,(40,40,40))
+    y+=toolH+1
+
+    #reundo
+    p.displayImage[y:y+toolH,-TOOL_WIDTH:]=(190,190,190)
+    cv2.putText(p.displayImage,'7:reundo',(p.displayImage.shape[0]-TOOL_WIDTH+3,y+toolH-3),cv2.FONT_HERSHEY_PLAIN,2.0,(40,40,40))
+    y+=toolH+1
+
     cv2.imshow("labeler",p.displayImage)
 
 def labelImage(imagePath,displayH,displayW):
@@ -251,9 +348,26 @@ def labelImage(imagePath,displayH,displayW):
             if p.mode != 'delete':
                 p.mode='delete'
                 drawToolbar(p)
+        elif key==54: #6 undo
+            undo(p)
+        elif key==55: #7 undo
+            redo(p)
 
-    #TODO scale?
-    print 'TODO scale'
+
+    idToIdxText={}
+    textBBs=[]
+    for id, (startX,startY,endX,endY,para) in p.textBBs.iteritems():
+        idToIdxText[id]=len(textBBs)
+        textBBs.append((int(round(startX/scale)),int(round(startY/scale)),int(round(endX/scale)),int(round(endY/scale)),para))
+    idToIdxField={}
+    fieldBBs=[]
+    for id, (startX,startY,endX,endY,para) in p.fieldBBs.iteritems():
+        idToIdxField[id]=len(fieldBBs)
+        fieldBBs.append((int(round(startX/scale)),int(round(startY/scale)),int(round(endX/scale)),int(round(endY/scale)),para))
+    pairing=[]
+    for text,field in p.pairing:
+        pairing.append((idToIdxText[text],idToIdxField[field]))
+
     return textBBs, fieldBBs, pairing
 
 print labelImage(sys.argv[1],int(sys.argv[2]),int(sys.argv[2]))
